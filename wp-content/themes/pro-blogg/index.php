b6b09b2674153aa6710d9e012d1c1687eb644f95
tzula <chuck@tzula.com>
2016-03-18 13:22:16 +0800

    test slide data

--- wp-content/themes/pro-blogg/index.php
+++ wp-content/themes/pro-blogg/index.php
@@ -15,7 +15,7 @@ get_header();
 				);
 			$the_query = new WP_Query( $args );  
 
-			$rows = $wpdb->get_results( "SELECT meta_value FROM wp_postmeta WHERE meta_key = 'show_in_slider' AND post_id = $post->ID" );
+			$rows = $wpdb->get_results( "SELECT meta_value FROM yy_postmeta WHERE meta_key = 'show_in_slider' AND post_id = $post->ID" );
 			var_dump($rows);exit;
 
 
